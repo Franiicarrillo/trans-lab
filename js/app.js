b0b612a8883f41c14231e611ea696db6f4e433d7
Francisca <Franiicarrillo@gmail.com>
2018-06-28 03:06:33 -0400

    agrego arreglos al estilo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
-let bring = {}
-fetch("http://www.psep.cl/api/Bip.php?&numberBip=666")
-.then(function(respuesta){
-  console.log(respuesta)
-  return respuesta.json();
-})
-.then(function(url){
-  bring.url=url
-  console.log(bring.url)
-})
+let direccionEmail=document.getElementById("mail").value
